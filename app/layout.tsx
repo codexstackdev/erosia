afacba0e4c27111632fa5cf812f78394a5ea2638
codexstackdev <zyntaxinc@gmail.com>
2026-01-19 16:43:22 +0800

    0.2 added protection

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,7 +43,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           <Toaster/>
-          {/* <DisableDevtools/> */}
+          <DisableDevtools/>
           {children}
         </ThemeProvider>
       </body>
